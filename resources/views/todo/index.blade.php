5f1cdf7f91448368a3b058f5f3761c84886d782e
Fendo181 <fendo181git@gmail.com>
2019-12-19 00:17:41 +0900

    色々整理をして、最低限のtodoフォームを作成した

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,9 +1,20 @@
 @extends('default')
 
 @section('content')
-    <h1>DoneTimer</h1>
-    <p>Doneリストを取得する</p>
-    @foreach ($todoList as $todo)
-        <li>{{ $todo->description }}</li>
-    @endforeach
+    <img src="{{asset('/img/DoneTimer.png')}}" class="img-fluid rounded mx-auto d-block" alt="Responsive image" width="300" height="300">
+    <form class="form-inline" action="/create" method="post">
+        <div class="mx-auto">
+            <select name="category" id="category">
+                <option value="research">調査</option>
+                <option value="system_design">システム設計</option>
+                <option value="coding">コーディング</option>
+                <option value="coordination">調整業</option>
+                <option value="writing">文章作成業</option>
+                <option value="others">そのほか</option>
+            </select>
+            <label class="sr-only" for="description">todo</label>
+            <input type="text" class="form-control mb-2 mr-sm-2" id="description" placeholder="今やる事">
+            <button type="submit" class="btn btn-primary mb-2">Submit</button>
+        </div>
+    </form>
 @endsection
