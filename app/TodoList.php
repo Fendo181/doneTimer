aec5769902ca5156ed1c9481fccaeeb942f93685
Fendo181 <fendo181git@gmail.com>
2019-12-19 03:20:32 +0900

    ミューテーを作成して、DBから取得する際は自動で日本語に変更するようにした

--- app/TodoList.php
+++ app/TodoList.php
@@ -18,6 +18,16 @@ class TodoList extends Model
             'others' => '#75a3a3',
         ];
 
+    protected $categoryLists =
+        [
+            'research' => '調査',
+            'system_design' => 'システム設計',
+            'coding' => 'コーディング',
+            'coordination' => '調整業',
+            'writing' => '文章作成業',
+            'others' => 'そのほか',
+        ];
+
     /**
      *
      * カテゴリー名に応じてカラーコードを返します。
@@ -27,5 +37,21 @@ class TodoList extends Model
     public function toCategoryColor($categoryName): string
     {
         return $this->colorCodeLists[$categoryName];
+    }
+
+    public function toJapaneseCategoryName($categoryName): string
+    {
+        return $this->categoryLists[$categoryName];
+    }
+
+    /**
+     *
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getCategoryAttribute($value)
+    {
+        return $this->toJapaneseCategoryName($value);
     }
 }
