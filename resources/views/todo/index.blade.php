5e0d4de7caa4e183e8e7b61c7572839637d2a7f5
Fendo181 <fendo181git@gmail.com>
2019-12-19 01:27:44 +0900

    form-controlを加えてフォームを整理した

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -4,7 +4,7 @@
     <img src="{{asset('/img/DoneTimer.png')}}" class="img-fluid rounded mx-auto d-block" alt="Responsive image" width="300" height="300">
     <form class="form-inline" action="/create" method="post">
         <div class="mx-auto">
-            <select name="category" id="category">
+            <select class="form-control" name="category" id="category">
                 <option value="research">調査</option>
                 <option value="system_design">システム設計</option>
                 <option value="coding">コーディング</option>
@@ -13,8 +13,8 @@
                 <option value="others">そのほか</option>
             </select>
             <label class="sr-only" for="description">todo</label>
-            <input type="text" class="form-control mb-2 mr-sm-2" id="description" placeholder="今やる事">
-            <button type="submit" class="btn btn-primary mb-2">Submit</button>
+            <input type="text" class="form-control" id="description" size="60" placeholder="今やる事">
+            <button type="submit" class="btn btn-primary">Submit</button>
         </div>
     </form>
 @endsection
