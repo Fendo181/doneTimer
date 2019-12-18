9fd93d437a4e0ab85925e48cc19984fb2b95f074
Fendo181 <fendo181git@gmail.com>
2019-12-18 10:27:01 +0900

    bladeテンプレートを使って共通化できるところは共通処理を入れるようにした

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DoneTimer</title>
-</head>
-<body>
-<h1>DoneTimer</h1>
-<p>Doneリストを取得する</p>
+@extends('default')
+
+@section('content')
+    <h1>DoneTimer</h1>
+    <p>Doneリストを取得する</p>
     @foreach ($todoList as $todo)
-    <li>{{ $todo->description }}</li>
+        <li>{{ $todo->description }}</li>
     @endforeach
-</body>
-</html>
+@endsection
