98a1e0f7cedf25e28e63a62d6843d08d24e45254
Fendo181 <fendo181git@gmail.com>
2019-12-19 04:59:28 +0900

    jsの処理だとわかるように先頭文字にprefixを付けた

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 // 削除時の処理
-$("#delete_todo").click(function(){
+$("#js-delete_todo").click(function(){
     if(confirm("本当に削除しますか？")){
         document.getElementById('delete_todo_form').submit()
     }else{
@@ -7,7 +7,8 @@ $("#delete_todo").click(function(){
     }
 });
 
-$("#edit_todo").click(function(){
+
+$("#js-edit_todo").click(function(){
     if(confirm("編集しますか？")){
         document.getElementById('edit_todo_form').submit()
     }else{
